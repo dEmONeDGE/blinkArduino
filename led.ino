37d6ee96cf9155b8127f262c5b8be279ba955963
Edgar E. Mamani A <edgar.eucebio.mamani.apaza@gmail.com>
2016-07-27 16:05:35 -0400

    Se añadio la comunicacion serial.

--- led.ino
+++ led.ino
@@ -1,12 +1,15 @@
 #define led 13
 
 void setup(){
+  Serial.begin(9600);
   pinMode(led, OUTPUT);
 }
 
 void loop(){
   digitalWrite(led, HIGH);
+  Serial.println("Led encendido.");
   delay(500);
   digitalWrite(led, LOW);
+  Serial.println("Led apagado");
   delay(500);
 }
